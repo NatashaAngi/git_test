680f13b4b3bf722b0c5f270fd679a8b26efbe603
Natasha Angi <96844887+NatashaAngi@users.noreply.github.com>
2026-01-31 16:10:31 -0300

    conclusão do desafio jokenpo

--- rock_paper_scissor/index.js
+++ rock_paper_scissor/index.js
@@ -1,35 +1,82 @@
 
+const opcoes = ['rock','paper','scissors']
 
-let entrada_usuario = prompt('choose rock paper or scissors')
+function get_playerchoice() {
 
-const objetos = {
-    rock: 1,
-    paper: 2,
-    scissors: 3
+   let choice = prompt('Choose: Rock, Paper, Scissors').toLocaleLowerCase()
+
+
+   if(opcoes.includes(choice)){
+
+    return choice
+   }
+
+   else{   
+        alert('Choose a valid option')
+        return get_playerchoice()
+
+   }
+
 }
 
-const nomes = {
-    1: "rock",
-    2: "paper",
-    3: "scissor"
+function get_pcchoice(){
+
+    let numero_escolhido = Math.floor(Math.random()*3)
+    const escolha_final = opcoes[numero_escolhido]
+
+    return escolha_final
 }
 
 
-function escolha_pc(){
-    const computer_choice = Math.floor(Math.random()*3)+1
-    const computer = computer_choice
-    return computer
+function compare_choices(playerchoice,pc_choice ){
+
+    let result =''
+
+    const regras = {
+        rock: 'scissors',
+        paper: 'rock',
+        scissors: 'paper'
+    }
+
+    if( pc_choice === playerchoice){
+        result = 'Tied'
+    }
+    
+    else if (pc_choice === regras[playerchoice]){
+        result = 'You WIN'}
+        
+    else{ result = 'PC WINS' }
+
+    return result
 }
 
-escolha_pc()
+function game(){
+   let player_score = 0
+   let pc_score = 0
 
-const computer = escolha_pc()
+    while( player_score < 5 && pc_score < 5){
+        
+    let playerchoice = get_playerchoice()
+    let pc_choice = get_pcchoice()
+    let result_match = compare_choices(playerchoice, pc_choice)
+        
+    if(result_match === 'You WIN' ){
+        player_score++
+    }
+    else if(result_match === 'PC WINS'){
+        pc_score++
+    }
 
-const escolha_usuario = objetos[entrada_usuario]
-const escolha_computador = nomes[computer]
+    console.log(`Placar atual: Jogador: ${player_score} x PC: ${pc_score}`)
+    }
 
-console.log(escolha_usuario)
-console.log(escolha_computador)
+    if(player_score > pc_score){
+        console.log(`The winner is the player with ${player_score} points.`)
+    }
+    else{ console.log(`The winner is the PC with ${pc_score} points.`)}
+}
+game()
+
 
 
 
